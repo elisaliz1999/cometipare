93fcbaf955e184736453644c6a6c8c127e6b7228
elisaliz1999 <elidelfrate29@gmail.com>
2022-06-28 23:33:32 +0200

    primo commit

---
+++ cometipare-be/src/main/java/it/elisa/cometipare/CometipareApplication.java
@@ -0,0 +1,13 @@
+package it.elisa.cometipare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CometipareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CometipareApplication.class, args);
+	}
+
+}
---
+++ cometipare-be/src/main/java/it/elisa/cometipare/models/D_Users.java
@@ -0,0 +1,21 @@
+package it.elisa.cometipare.models;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data //crea da se getter e setter, costruttore vuoto, costruttore con tutte le variabili
+public class D_Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String username;
+    private String password;
+    private String nome;
+    private String cognome;
+}
